ee10e7ec3d7376a6fc5f0c8dd5c3775a47558729
Victor Rafael Santana Pimentel <victorrafaelsantana@hotmail.es>
2024-09-20 20:32:21 -0600

    These are the homeworks of week 1.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
+        // This just writes the message
         Console.WriteLine("Hello Prep2 World!");
-    }
-}
+        // This writes an extra space line
+        Console.WriteLine();
+        
+        // This is part 2 of the prep hw
+        Console.Write("What is your grage percentage? ");
+        // This reads the input
+        string gradeString = Console.ReadLine();
+        // This converts the input from String to Integer
+        int grade = int.Parse(gradeString);
+
+        // Declaring variable
+        string letter = "";
+        
+        // This will the chain of if statements that checks the grade
+        if (grade >= 90)
+        {
+            letter = "A";
+            //Console.WriteLine("You have an A.");
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+            //Console.WriteLine("You have an B.");
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+            //Console.WriteLine("You have an C.");
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+            //Console.WriteLine("You have an D.");
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+            //Console.WriteLine("You have an F.");
+        }
+
+        if (grade >= 70) 
+        {   
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class.");
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+    } // END OF STATIC VOID
+
+} // END OF CLASS PROGRAM
